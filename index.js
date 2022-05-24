1e293126bed15603823dc7f5b52ac41a3b1ab84c
ayman <fadliayman10@gmail.com>
2022-05-24 19:02:26 +0100

    added : core

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import articlesRoutes from './routes/articles.js';
 import commentsRoutes from './routes/comments.js';
 import usersRoutes from './routes/users.js';
-
+import cors from 'cors';
 const app = express();
 const port = "3000";
 app.use(cors({
